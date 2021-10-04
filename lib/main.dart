522912f5a96c1d4178a3968ddec3e6e53485fcf0
Jookate0726 <69692093+Jookate0726@users.noreply.github.com>
2021-10-04 09:54:00 +0530

    added packages to structure the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,8 @@
 import 'package:flutter/material.dart';
+import 'config/myapp.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: MyApp(),
-      debugShowCheckedModeBanner: false,
-    ),
+    MyApp(),
   );
-}
-
-class MyApp extends StatefulWidget {
-  MyApp({Key key}) : super(key: key);
-
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
 }
---
+++ lib/providers/wrapper.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_firebase/screens/home/home.dart';
+
+class Wrapper extends StatelessWidget {
+  const Wrapper({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // if signed in return home, else authenticate
+    return Home();
+  }
+}
